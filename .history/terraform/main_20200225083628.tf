bb7fe9dc9ba1c92c67a1804720e240b4f03d55eb
mmbl <mmblsc@gmail.com>
2020-02-25 08:39:24 +0300

    terraform описание готово

---
+++ .history/terraform/main_20200225083628.tf
@@ -0,0 +1,128 @@
+################################################################################
+# PROVIDERS
+################################################################################
+terraform {
+  required_version = ">= 0.12"
+}
+
+# instance the provider
+provider "libvirt" {
+  uri = "qemu:///system"
+}
+
+################################################################################
+# VAR
+################################################################################
+
+variable "ANSIBLE_USER" {
+  default = "ansible"
+  type    = string
+}
+variable "SSH_KEY_PATH" {
+  type    = string
+  default = "~/.ssh/id_rsa.pub"
+}
+
+variable "LIBVIRT_POOL_DIR" {
+  type    = string
+  default = "/home/d/virtlab/vm_weblogic"
+}
+
+variable "RH81_IMG_URL_64" {
+  type    = string
+  default = "/home/d/repository/rhel-8.1-x86_64-kvm.qcow2"
+}
+
+resource "libvirt_pool" "weblogic_env" {
+  name = "weblogic_env"
+  type = "dir"
+  path = abspath("${var.LIBVIRT_POOL_DIR}")
+}
+
+resource "libvirt_network" "weblogic_network" {
+  name      = "weblogic_network"
+  addresses = ["10.10.30.0/24"]
+
+  dns {
+    enabled    = true
+    local_only = true
+  }
+}
+################################################################################
+# RESOURCES
+################################################################################
+data "template_file" "network_config" {
+  template = file("${path.module}/network_config.yaml")
+}
+
+resource "libvirt_volume" "rh_disk" {
+  name   = "rh_disk"
+  pool   = libvirt_pool.weblogic_env.name
+  source = var.RH81_IMG_URL_64
+  format = "qcow2"
+}
+#################################################################
+# VM
+#################################################################
+data "template_file" "wls_user_data" {
+  template = file("${path.module}/cloud_init.yaml")
+  vars = {
+    # пользователи из переменных выше - технический долг, при большом количестве добавляемых сущностей
+    # реализовать циклический перебор или как-то вложить в одну переменную
+    ANSIBLE_USER: var.ANSIBLE_USER
+    SSH_KEY  = file(var.SSH_KEY_PATH)
+    HOSTNAME = "wls"
+  }
+}
+
+resource "libvirt_cloudinit_disk" "cloudinit" {
+  name           = "wls_cloudinit.iso"
+  user_data      = data.template_file.wls_user_data.rendered
+  network_config = data.template_file.network_config.rendered
+  pool           = libvirt_pool.weblogic_env.name
+}
+
+resource "libvirt_volume" "wls_rh_disk" {
+  name           = "wls_rh_disk_${count.index}"
+  base_volume_id = libvirt_volume.rh_disk.id
+  pool           = libvirt_pool.weblogic_env.name
+  count          = 2
+}
+
+resource "libvirt_domain" "wls_rh_host" {
+  name       = "wls_rh_host_${count.index}"
+  memory     = "512"
+  vcpu       = 1
+  autostart  = true
+  qemu_agent = true
+
+  count = 2
+
+  cloudinit = libvirt_cloudinit_disk.cloudinit.id
+
+  network_interface {
+    network_name   = libvirt_network.weblogic_network.name
+    hostname       = "wls-${count.index}"
+    wait_for_lease = true
+  }
+
+  disk {
+    volume_id = element(libvirt_volume.wls_rh_disk.*.id, count.index)
+  }
+
+  console {
+    type        = "pty"
+    target_port = "0"
+    target_type = "serial"
+  }
+
+  console {
+    type        = "pty"
+    target_type = "virtio"
+    target_port = "1"
+  }
+
+}
+output "vm_weblogic" {
+  value = libvirt_domain.wls_rh_host.*.network_interface.0.addresses
+}
---
+++ create_inventory_file.py
@@ -0,0 +1,37 @@
+"""Формирование файла host"""
+
+import pathlib
+import os
+import json
+import yaml
+from functools import reduce
+import re
+__path__ = pathlib.Path(__file__)
+
+tfstate = []
+p = pathlib.Path('.')
+for f in p.rglob('*.tfstate'):
+    # костыль: купирует файлы из .history
+    if re.match(r'^\.*', str(f)).group(0):
+        continue
+    tfstate.append(f.absolute())
+
+hosts = {}
+
+for path in tfstate:
+    with open(path, "r") as json_file:
+        js_ob = json.load(json_file)
+        for k, v in js_ob.get('outputs').items():
+            if isinstance(v.get('value')[0], list):
+                # При создании однотипных серверв с использованием count генерируется список списков
+                hosts.update({k: reduce(lambda x, y: x + y, v.get('value'))})
+            else:
+                hosts.update({k: v.get('value')})
+ls = []
+for k, v in hosts.items():
+    # фильтрация local ip, убирает IPv6 и не принадлежащие 10.х.х.х
+    l = [ip for ip in v if '10.' in ip]
+    ls.append("[{}]\n{}\n".format(k, '\n'.join(l)))
+
+with open('terraform_inventory', 'w') as f:
+    f.write('\n'.join(ls))
